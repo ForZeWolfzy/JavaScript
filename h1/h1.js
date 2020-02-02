7eca5f12844ad2cda2493092a0421c8b9ecf3784
ForZeWolfzy <gbolwerk@roc-dev.com>
2020-02-02 15:41:22 +0100

    h1

---
+++ h1/h1.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+
+var a = 10;
+if (a > 9) {
+    console.log("als 'a' kleiner was dan 10 zou je dezen text niet zien")
+}
+for (var i = 0; i < 10 ; i++) {
+    console.log("i heeft nu een waarde van: " + i)
+}
+var mijnSchoen = {
+    merk: "Nike",
+    Type: "Airmax",
+    kleur: "Blauw"
+}
+console.log(mijnSchoen.merk)
